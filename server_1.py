bdd6f84ab701511b15cf81addcfae3ee6ab60fde
dmitri1998171 <dmitri1998171@icloud.com>
2023-12-04 18:42:22 +0700

    add Server_1 change window title

--- server_1.py
+++ server_1.py
@@ -1,21 +1,26 @@
 import sys, socket, datetime
 from threading import Thread 
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import uic
+from PyQt5.QtCore import * 
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 from server_design import Ui_MainWindow  # импорт сгенерированного файла
 
 stop = False
 sock = None
 app = None
+title =''
+changeWinTitleStatus = 0
 
 def collectInfo():
     pass
 
-def handle_connection(sock, addr):  # New
+def handle_connection(sock, addr, window):
     with sock:
         print("Connected by", addr)
         
         while not stop:
-            # Receive
+            # Получение сообщения от Клиента
             try:
                 data = sock.recv(1024).decode()
 
@@ -27,22 +32,36 @@ def handle_connection(sock, addr):  # New
             
             if not data:
                 break
-            
+
+            # Добавление времени в ответное сообщение 
+            now = datetime.datetime.now()
+            answer = now.strftime("%d-%m-%Y %H:%M:%S")
+
+            # Если пришел запрос на изменение заголовка
             if(data.find(" | ") != -1):
                 data = data.split(" | ")
                 win_geometry = data[0]
+                
+                global title
                 title = data[1]
-                print(f"\nwin_geometry: {win_geometry}\ntitle: {title}\n")
-
-            now = datetime.datetime.now()
-            data = now.strftime("%d-%m-%Y %H:%M:%S")
+                
+                window.changeTitle()
+                QThread.msleep(100)
+                print("TITLE: ", window.windowTitle())
+                
+                # Проверка смены заголовка
+                if(window.windowTitle() == title):
+                    answer += " Смена заголовка: Успешно"
+                else:
+                    answer += " Смена заголовка: Ошибка"
+                
             # data += collectInfo()
             
-            # Send
-            print(f"Send: {data} to: {addr}")
+            # Отправка ответа Клиенту
+            print(f"Send: {answer} to: {addr}")
             
             try:
-                sock.sendall(data.encode())
+                sock.sendall(answer.encode())
             
             except ConnectionError:
                 print(f"Client suddenly closed, cannot send")
@@ -50,16 +69,49 @@ def handle_connection(sock, addr):  # New
         
         print("Disconnected by", addr)
 
-class mywindow(QtWidgets.QMainWindow):
+# Класс, необходимый для смены заголовка окна сервера
+class generate_insert_frame(QThread):
+    threadSignal = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        global title
+
+        self.threadSignal.emit(title)
+        self.msleep(100)
+        self.stop()
+
+    def stop(self):
+        self.quit()
+
+class mywindow(QMainWindow):
     def __init__(self):
         super(mywindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+    def changeTitle(self):
+    # Механизм сигналов/слотов передает информацию в главный поток из дочернего 
+    # так как setWindowTitle() работает только из главного потока
+        self.thread = generate_insert_frame() 
+        self.thread.threadSignal.connect(self.setWindowTitle)
+        self.thread.start()
+
+    def checkWindowTitle(self):
+        global title
+        global changeWinTitleStatus
+
+        if(title == self.windowTitle()):
+            changeWinTitleStatus = 1
+
+        changeWinTitleStatus = 0
+
 class ServerThread(Thread):
-    def __init__(self,window): 
+    def __init__(self, window): 
         Thread.__init__(self) 
-        self.window=window
+        self.window = window
 
     def run(self): 
         HOST = "localhost"
@@ -76,7 +128,7 @@ class ServerThread(Thread):
 
                     print("Waiting for connection...")
                     sock, addr = serv_sock.accept()
-                    t = Thread(target=handle_connection, args=(sock, addr))  # New
+                    t = Thread(target=handle_connection, args=(sock, addr, window))  # New
                     t.start()
 
             except KeyboardInterrupt:
@@ -88,10 +140,10 @@ class ServerThread(Thread):
 
 
 if __name__ == '__main__':
-    lockfile = QtCore.QLockFile(QtCore.QDir.tempPath() + '/server_1.lock')
+    lockfile = QLockFile(QDir.tempPath() + '/server_1.lock')
 
     if lockfile.tryLock(100):
-        app = QtWidgets.QApplication(sys.argv)
+        app = QApplication(sys.argv)
         
         window = mywindow()
         serverThread=ServerThread(window)
